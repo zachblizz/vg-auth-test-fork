2f2f26d73777b6b2540f6533c5f08186144e7da8
Jeff Swartz <jeff.swartz@vonage.com>
2022-10-10 11:24:58 -0700

    Add eslint, use AirBnB ES2020

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'func-names': 0,
+    'no-console': 0,
+    'no-restricted-globals': 0,
+  },
+};
--- public/js/helloworld.js
+++ public/js/helloworld.js
@@ -1,148 +1,148 @@
 /* global OT, appId, sessionId, token */
 
-var session = OT.initSession(appId, sessionId);
+const session = OT.initSession(appId, sessionId);
 
-var publisher = OT.initPublisher('publisher');
-var streamId;
-var archiveId;
-var logElement;
-var logDiv;
+const publisher = OT.initPublisher('publisher');
+let streamId;
+let archiveId;
+let logPre;
+let logDiv;
 
-var log = function (str) {
-  var logStr = new Date().toISOString() + ': ' + str + '\n';
+const log = function (str) {
+  const logStr = `${new Date().toISOString()}: ${str}\n`;
   console.log(logStr);
   logPre.innerText += logStr;
   logDiv.scrollTop = logDiv.scrollHeight;
-}
+};
 
 session.on({
-  sessionConnected: function () {
-    log('connected to session ' + sessionId);
+  sessionConnected() {
+    log(`connected to session ${sessionId}`);
     session.publish(publisher);
   },
 
-  streamCreated: function (event) {
-    log('new subscriber stream ' + event.stream.id);
+  streamCreated(event) {
+    log(`new subscriber stream ${event.stream.id}`);
     session.subscribe(event.stream, 'streams-container', { insertMode: 'append' });
   },
-  streamDestroyed: function (event) {
-    log('subscriber stream e' + event.stream.id);
+  streamDestroyed(event) {
+    log(`subscriber stream e${event.stream.id}`);
     session.subscribe(event.stream, 'streams-container', { insertMode: 'append' });
   },
-  signal: function(e) {
-    log('signal data:' + e.data + ' -- type: ' + e.type);
+  signal(e) {
+    log(`signal data:${e.data} -- type: ${e.type}`);
   },
-  
-  archiveStarted: function(e) {
-    log('archiveStarted ' + e.id);
+
+  archiveStarted(e) {
+    log(`archiveStarted ${e.id}`);
     archiveId = e.id;
   },
-  
-  archiveStopped: function(e) {
-    log('archiveStopped ' + e.id);
+
+  archiveStopped(e) {
+    log(`archiveStopped ${e.id}`);
   },
-  
-  sessionDisconnected: function(e) {
+
+  sessionDisconnected() {
     log('sessionDisconnected');
-  }
+  },
 });
 
-publisher.on('streamCreated', function (event) {
-  log('new published stream ' + event.stream.id);
+publisher.on('streamCreated', (event) => {
+  log(`new published stream ${event.stream.id}`);
   streamId = event.stream.id;
 });
 
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded', () => {
   logPre = document.getElementById('log-pre');
   logDiv = document.getElementById('log-div');
-  document.getElementById('start-archive-btn').addEventListener('click', function () {
-    log('startArchive')
-    fetch('/startArchive/' + sessionId + location.search, {
-      method: 'get'
+  document.getElementById('start-archive-btn').addEventListener('click', () => {
+    log('startArchive');
+    fetch(`/startArchive/${sessionId}${location.search}`, {
+      method: 'get',
     })
-      .then(function (response) { return response.json(); })
-      .then(function (data) {
+      .then((response) => response.json())
+      .then((data) => {
         archiveId = data.id;
         log(JSON.stringify(data, null, 2));
       });
   });
 
-  document.getElementById('stop-archive-btn').addEventListener('click', function () {
-    log('stopArchive ' + archiveId);
-    fetch('/stopArchive/' +  archiveId + location.search, {
-      method: 'get'
+  document.getElementById('stop-archive-btn').addEventListener('click', () => {
+    log(`stopArchive ${archiveId}`);
+    fetch(`/stopArchive/${archiveId}${location.search}`, {
+      method: 'get',
     })
-      .then(function (response) { return response.json(); })
-      .then(function (data) {
+      .then((response) => response.json())
+      .then((data) => {
         archiveId = data.id;
         log(JSON.stringify(data, null, 2));
       });
   });
 
-  document.getElementById('force-disconnect-btn').addEventListener('click', function () {
-    fetch('/forceDisconnect/' + sessionId + '/' +  session.connection.id + location.search, {
-      method: 'get'
-    }).then(function (response) { log('forced to disconnect'); });
+  document.getElementById('force-disconnect-btn').addEventListener('click', () => {
+    fetch(`/forceDisconnect/${sessionId}/${session.connection.id}${location.search}`, {
+      method: 'get',
+    }).then(() => { log('forced to disconnect'); });
   });
 
-  document.getElementById('force-mute-all-btn').addEventListener('click', function () {
-    fetch('/forceMuteAll/' + sessionId + location.search, {
-      method: 'get'
-    }).then(function (response) { log('session muted'); });
+  document.getElementById('force-mute-all-btn').addEventListener('click', () => {
+    fetch(`/forceMuteAll/${sessionId}${location.search}`, {
+      method: 'get',
+    }).then(() => { log('session muted'); });
   });
 
-  document.getElementById('force-mute-stream-btn').addEventListener('click', function () {
-    fetch('/forceMuteStream/' + sessionId + '/' + streamId + location.search, {
-      method: 'get'
-    }).then(function (response) { log('stream muted'); });
+  document.getElementById('force-mute-stream-btn').addEventListener('click', () => {
+    fetch(`/forceMuteStream/${sessionId}/${streamId}${location.search}`, {
+      method: 'get',
+    }).then(() => { log('stream muted'); });
   });
 
-  document.getElementById('disable-force-mute-btn').addEventListener('click', function () {
-    fetch('/disableForceMute/' + sessionId + location.search, {
-      method: 'get'
-    }).then(function (response) { log('force mute disabled'); });
+  document.getElementById('disable-force-mute-btn').addEventListener('click', () => {
+    fetch(`/disableForceMute/${sessionId}${location.search}`, {
+      method: 'get',
+    }).then(() => { log('force mute disabled'); });
   });
 
-  document.getElementById('signal-me-btn').addEventListener('click', function () {
-    fetch('/signalConnection/' + sessionId + '/' + session.connection.id + location.search, {
-      method: 'get'
+  document.getElementById('signal-me-btn').addEventListener('click', () => {
+    fetch(`/signalConnection/${sessionId}/${session.connection.id}${location.search}`, {
+      method: 'get',
     });
   });
 
-  document.getElementById('signal-all-btn').addEventListener('click', function () {
-    console.log(23342 + '/signalAll/' + sessionId)
-    fetch('/signalAll/' + sessionId + location.search, {
-      method: 'get'
+  document.getElementById('signal-all-btn').addEventListener('click', () => {
+    console.log(`${23342}/signalAll/${sessionId}`);
+    fetch(`/signalAll/${sessionId}${location.search}`, {
+      method: 'get',
     });
   });
 
-  document.getElementById('list-streams-btn').addEventListener('click', function () {
-    fetch('/listStreams/' + sessionId + location.search, {
-      method: 'get'
+  document.getElementById('list-streams-btn').addEventListener('click', () => {
+    fetch(`/listStreams/${sessionId}${location.search}`, {
+      method: 'get',
     })
-      .then(function (response) { return response.json(); })
-      .then(function (data) { log(JSON.stringify(data, null, 2)); });
+      .then((response) => response.json())
+      .then((data) => { log(JSON.stringify(data, null, 2)); });
   });
 
-  document.getElementById('get-stream-btn').addEventListener('click', function () {
-    fetch('/getStream/' + sessionId + '/' + streamId + location.search, {
-      method: 'get'
+  document.getElementById('get-stream-btn').addEventListener('click', () => {
+    fetch(`/getStream/${sessionId}/${streamId}${location.search}`, {
+      method: 'get',
     })
-      .then(function (response) { return response.json(); })
-      .then(function (data) { log(JSON.stringify(data, null, 2)); });
+      .then((response) => response.json())
+      .then((data) => { log(JSON.stringify(data, null, 2)); });
   });
 
-  document.getElementById('list-archives-btn').addEventListener('click', function () {
-    fetch('/listArchives/' + sessionId + location.search, {
-      method: 'get'
+  document.getElementById('list-archives-btn').addEventListener('click', () => {
+    fetch(`/listArchives/${sessionId}${location.search}`, {
+      method: 'get',
     })
-      .then(function (response) { return response.json(); })
-      .then(function (data) { log(JSON.stringify(data, null, 2)); });
+      .then((response) => response.json())
+      .then((data) => { log(JSON.stringify(data, null, 2)); });
   });
 
-  document.getElementById('set-class-list-btn').addEventListener('click', function () {
-    fetch('/setStreamClassLists/' + sessionId + '/' + streamId + location.search, {
-      method: 'get'
+  document.getElementById('set-class-list-btn').addEventListener('click', () => {
+    fetch(`/setStreamClassLists/${sessionId}/${streamId}${location.search}`, {
+      method: 'get',
     })
       .then(log('stream class list updated'));
   });
